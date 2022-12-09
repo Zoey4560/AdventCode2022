620e823676bcd6a8e3d6a886d1c1829b22fdd841
zoey4560 <1054177+Zoey4560@users.noreply.github.com>
2022-12-08 23:02:49 -0800

    2.2

--- day_2.py
+++ day_2.py
@@ -1,10 +1,10 @@
+r"""Going object oriented-ish for this, if we're gonna be doing gameplay
+>>> play_tourney_v1('A Y\nB X\nC Z')
+15
+>>> play_tourney_v2('A Y\nB X\nC Z')
+12
 """
-v0: Going object oriented for this, if we're gonna be doing gameplay
-v0.1                    ^ish
 
-# useful for ipython dev
-# > from importlib import reload
-"""
 from enum import auto, IntEnum
 
 """Enums acting both as enums and stores for point values for scoring `the shape you select` and `the outcome of the round`"""
@@ -13,12 +13,20 @@ class Shape(IntEnum):
     paper = 2
     scisors = 3
 
-    def beats(self, their_move):
+    def beats(self):
         return {
             Shape.rock: Shape.scisors,
             Shape.scisors: Shape.paper,
             Shape.paper: Shape.rock
-        }[self] == their_move
+        }[self]
+    
+    def needs(self, desired_outcome):
+        """poorly named method that tells what you need to play in response to Shape to cause the desired outcome"""
+        if desired_outcome is Outcome.draw:
+            return self
+        if desired_outcome is Outcome.loss:
+            return self.beats()
+        return self.beats().beats()
 
 
 class Outcome(IntEnum):
@@ -30,12 +38,14 @@ class Outcome(IntEnum):
     def of_round(cls, their_move, your_move):
         if your_move is their_move:
             return cls.draw
-        elif your_move.beats(their_move):
+        elif your_move.beats() == their_move:
             return cls.win
         return cls.loss
 
 
-def _decode(code):
+"""v1 and v2 for part 1 and 2 of the problem"""
+
+def _decode_v1(code):
     return {
         'A': Shape.rock,
         'X': Shape.rock,
@@ -45,9 +55,36 @@ def _decode(code):
         'Z': Shape.scisors,
     }[code]
 
+def _decode_v2(code):
+    return {
+        'A': Shape.rock,
+        'X': Outcome.loss,
+        'B': Shape.paper,
+        'Y': Outcome.draw,
+        'C': Shape.scisors,
+        'Z': Outcome.win,
+    }[code]
 
-def play_tourney(_in):
+def decode_input(_in, decoder):
     coded_moveset = [x.split(' ') for x in _in.split('\n')]
-    moveset = [ (_decode(them), _decode(you)) for (them, you) in coded_moveset]
-    score = sum([ you + Outcome.of_round(them, you) for them, you in moveset ])
-    return score
+    moveset = [ (decoder(x), decoder(y)) for (x, y) in coded_moveset]
+    return moveset
+
+def score(moveset):
+    return sum([ you + Outcome.of_round(them, you) for them, you in moveset ])
+
+def play_tourney_v1(_in):
+    moveset = decode_input(_in, _decode_v1)
+    return score(moveset)
+
+def play_tourney_v2(_in):
+    strategy_guide = decode_input(_in, _decode_v2)
+    moveset = [ (them, them.needs(outcome)) for (them, outcome) in strategy_guide]
+    return score(moveset)
+
+
+if __name__ == "__main__":
+    with open('input/2.txt') as fh:
+        _in = fh.read()
+        print(f'Part 1: {play_tourney_v1(_in)}')
+        print(f'Part 2: {play_tourney_v2(_in)}')
