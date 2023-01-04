0d93217876fd7ccdf6ba1f5dc4b8c8e99856641b
Zoey4560 <1054177+Zoey4560@users.noreply.github.com>
2023-01-03 20:54:50 -0800

    11.1

--- day_11.py
+++ day_11.py
@@ -7,35 +7,73 @@ r""" Day 11
 
 
 """
-
+from typing import List
 
 
 class Monkey:
-    items = []
+    items: List[int]
     operation = None
-    test = None
-    true_throw = None
-    false_throw = None
+    test: int
+    true_throw: int
+    false_throw: int
+    inspection_count: int = 0
+
+    def __repr__(self) -> str:
+        return f'<Monkey {self.items}>'
 
     def __init__(self, _monkey_string: str):
         _, item_str, op_str, *test_strs = _monkey_string.split('\n')
-        print(item_str)
-        print(op_str)
-        print(test_strs)
 
+        self.items = [int(item) for item in item_str[18:].split(', ')]
+
+        self.operation = self.generate_operation(op_str)
+
+        self.test = int(test_strs[0][21:])
+        self.true_throw = int(test_strs[1][29:])
+        self.false_throw = int(test_strs[2][30:])
+
+    def throw_items(self):
+        for item in self.items:
+            item = self.operation(item)
+            self.inspection_count += 1
+            item = item // 3
+            target = self.true_throw if item % self.test == 0 else self.false_throw
+            yield target, item
+        self.items = []
+
+    @staticmethod
+    def generate_operation(op_str):
+        def func(item: int):
+            if op_str[25:] == 'old':
+                factor = item
+            else:
+                factor = int(op_str[25:])
+            if op_str[23] == '+':
+                return item + factor
+            else: # == '*'
+                return item * factor
+        return func
 
 
 def solve_1(_in):
+
     monkeys = []
     for monkey_string in _in.split('\n\n'):
-        return Monkey(monkey_string)
+        monkeys.append(Monkey(monkey_string))
+
+    for _ in range(20):
+        # play 20 rounds
+        for monkey in monkeys:
+            for target, item in monkey.throw_items():
+                monkeys[target].items.append(item)
+    
+    inspection_counts = [m.inspection_count for m in monkeys]
+    inspection_counts.sort(reverse=True)
+    return inspection_counts[0] * inspection_counts[1]
 
 
 def solve_2(_in):
     pass
-
-
-
 
 
 if __name__ == "__main__":
