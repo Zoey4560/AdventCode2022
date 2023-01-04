bfcf698b452719114879711668d25b394a2ad797
Zoey4560 <1054177+Zoey4560@users.noreply.github.com>
2023-01-03 21:34:40 -0800

    11.monkeytypes

--- day_11.py
+++ day_11.py
@@ -3,15 +3,24 @@ r""" Day 11
 >>> sample_input = 'Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1'
 >>> solve_1(sample_input)
 10605
-
-
+# >>> solve_2(sample_input)
+2713310158
 
 """
 from typing import List
+from enum import auto, Enum
+
+class MonkeyType(Enum):
+    ADD = auto()
+    MULT = auto()
+    SQ = auto()
+
 
 
 class Monkey:
+
     items: List[int]
+    monkey_type: MonkeyType
     operation = None
     test: int
     true_throw: int
@@ -21,48 +30,55 @@ class Monkey:
     def __repr__(self) -> str:
         return f'<Monkey {self.items}>'
 
-    def __init__(self, _monkey_string: str):
+    def __init__(self, _monkey_string: str, relief=False):
         _, item_str, op_str, *test_strs = _monkey_string.split('\n')
 
         self.items = [int(item) for item in item_str[18:].split(', ')]
 
-        self.operation = self.generate_operation(op_str)
+        self.monkey_type, self.operation = self._get_monkey_type_and_op(op_str)
 
         self.test = int(test_strs[0][21:])
         self.true_throw = int(test_strs[1][29:])
         self.false_throw = int(test_strs[2][30:])
 
+        self.relief = relief
+
     def throw_items(self):
         for item in self.items:
             item = self.operation(item)
             self.inspection_count += 1
-            item = item // 3
+            if self.relief:
+                item = item // 3
+            else:
+                pass
+                # TODO some smart math?
             target = self.true_throw if item % self.test == 0 else self.false_throw
             yield target, item
         self.items = []
 
+
     @staticmethod
-    def generate_operation(op_str):
-        def func(item: int):
-            if op_str[25:] == 'old':
-                factor = item
-            else:
-                factor = int(op_str[25:])
+    def _get_monkey_type_and_op(op_str):
+        if op_str[25:] == 'old':  # assumes that there's only `old * old` and not `old + old`
+            return MonkeyType.SQ, lambda x: x * x
+        else:
+            factor = int(op_str[25:])
             if op_str[23] == '+':
-                return item + factor
+                return MonkeyType.ADD, lambda x: x + factor
             else: # == '*'
-                return item * factor
-        return func
+                return MonkeyType.MULT, lambda x: x * factor
 
 
-def solve_1(_in):
 
+
+def solve(_in, rounds, relief):
     monkeys = []
     for monkey_string in _in.split('\n\n'):
-        monkeys.append(Monkey(monkey_string))
+        monkeys.append(Monkey(monkey_string, relief=relief))
 
-    for _ in range(20):
-        # play 20 rounds
+    for r in range(rounds):
+        # play rounds
+        print(f'Round {r}')
         for monkey in monkeys:
             for target, item in monkey.throw_items():
                 monkeys[target].items.append(item)
@@ -71,9 +87,12 @@ def solve_1(_in):
     inspection_counts.sort(reverse=True)
     return inspection_counts[0] * inspection_counts[1]
 
+def solve_1(_in):
+    return solve(_in, 20, True)
+    
 
 def solve_2(_in):
-    pass
+    return solve(_in, 10_000, False)
 
 
 if __name__ == "__main__":
